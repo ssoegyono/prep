5cda2244244a2c88e70103b607ce036bd82bc169
steve soegyono <steve.soegyono@towerswatson.com>
2013-06-20 15:17:54 +1000

    Pushing new changes

--- source/prep.specs/CalculatorSpecs.cs
+++ source/prep.specs/CalculatorSpecs.cs
@@ -6,13 +6,16 @@ namespace prep.specs
   {
     public class when_adding_two_numbers
     {
+      static int result;
+
       Because b = () =>
         result = Calculator.add(2, 3);
 
       It should_return_the_sum = () =>
         result.ShouldEqual(5);
 
-      static int result;
     }
+
+
   }
 }
--- source/prep/Calculator.cs
+++ source/prep/Calculator.cs
@@ -4,7 +4,8 @@
   {
     public static int add(int first, int second)
     {
-      throw new System.NotImplementedException();
+        return first + second;
     }
+
   }
 }
