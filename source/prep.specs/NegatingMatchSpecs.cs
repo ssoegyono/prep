bb12c94f59e33acddfa069a25b94f0bdcba01233
ssoegyono <steve.soegyono@towerswatson.com>
2013-06-21 07:40:42 +1000

    Pushing new changes

---
+++ source/prep.specs/NegatingMatchSpecs.cs
@@ -0,0 +1,63 @@
+﻿using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+using prep.utility.searching;
+
+namespace prep.specs
+{
+  public class NegatingMatchSpecs
+  {
+      public abstract class concern : Observes<NegatingMatch<AnynomousItem>>
+      {
+      }
+
+      public class when_matches_true_return_false : concern
+      {
+          Establish c = () =>
+          {
+              match_to_negate = depends.on<IMatchA<AnynomousItem>>();
+              dummyItem = fake.an<AnynomousItem>();
+              match_to_negate.setup(x => x.matches(dummyItem)).Return(isMatched);
+          };
+
+
+          private Because b = () => result = sut.matches(dummyItem);
+
+          It should_return_false = () =>
+            result.ShouldEqual(!isMatched);
+
+          static IMatchA<AnynomousItem> match_to_negate;
+          static bool result;
+          static AnynomousItem dummyItem;
+          static bool isMatched = true;
+
+      }
+
+      public class when_matches_false_return_true : concern
+      {
+          Establish c = () =>
+          {
+              match_to_negate = depends.on<IMatchA<AnynomousItem>>();
+              dummyItem = fake.an<AnynomousItem>();
+              match_to_negate.setup(x => x.matches(dummyItem)).Return(isMatched);
+          };
+
+
+          private Because b = () => result = sut.matches(dummyItem);
+
+          It should_return_true = () =>
+            result.ShouldEqual(!isMatched);
+
+          static IMatchA<AnynomousItem> match_to_negate;
+          static bool result;
+          static AnynomousItem dummyItem;
+          static bool isMatched = true;
+
+      }
+
+  }
+
+  public class AnynomousItem
+  {
+  }
+}
